b4be74af90cd62a0336c0b23c53be41b0cecd840
1chooo <hugo970217@gmail.com>
2022-10-25 22:22:18 +0800

    Add: Note, Rearrange file

--- README.md
+++ README.md
@@ -8,3 +8,18 @@ $ conda install -c conda-forge netcdf4
 $ conda install pandas
 $ conda install -c anaconda xarray
 ```
+
+## The record of Analysis part.
+
+* [x] monthly_mean
+  * [x] $CO_2$
+  * [x] $N_{2}O$
+  * [x] $SF_6$
+  * [x] $CH_4$
+
+
+* [x] recent_monthly_mean
+  * [x] $CO_2$
+  * [x] $N_{2}O$
+  * [x] $SF_6$
+  * [x] $CH_4$
--- Term/CO2/test.py
+++ Term/CO2/test.py
@@ -1,10 +1,13 @@
 from Load import Tool
 
 data_path = r"/Users/linchunho/Developer/ATM-Chem/src/data/monthly_mean/"
+imgs_path = r"/Users/linchunho/Developer/ATM-Chem/src/imgs/CO2/"
 type = "co2"
 skip_rows = 53
 data_type = ["year", "month", "date", "avg", "de-s", "day", "std", "unc"]
 
-data = Tool.loadData(data_path, type, skip_rows, data_type)
+data = Tool.load_data(data_path, type, skip_rows, data_type)
+
+water_mark = Tool.add_water_mark(imgs_path)
 
 print(data)
